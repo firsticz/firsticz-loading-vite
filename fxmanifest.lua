4d2289154d598587887487f3fedb332ec32a00f7
firsticz <first@thai.run>
2022-07-23 20:09:05 +0700

    update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,11 @@ author "firsticz"
 description "FirsticZ LoadingScreen"
 
 files {
-    'index.html'
+    'index.html',
+    'assets/index.7bb4bb1f.css',
+    'assets/index.cede550a.js',
+    'assets/song.f4e4deec.mp3',
+    'gta610.jpg'
 }
 
 loadscreen 'index.html'
