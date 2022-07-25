53c380db7f799caed4f8ea1d973f0c20138edfeb
firsticz <first@thai.run>
2022-07-25 23:00:23 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import PlayPause from './components/PlayPause'
 function App() {
   const [showPlayButton, setshowPlayButton] = useState(false)
   const [playing, setPlaying] = useState(true);
+  const [loading, setLoading] = useState(0)
 
   const audioRef = useRef(new Audio(soundUrl));
   const btnRef = useRef()
@@ -44,7 +45,62 @@ function App() {
     console.log('keydown');
   }
 
+  // PROGRESS
+  let states = {
+    'INIT_BEFORE_MAP_LOADED': {
+      count: 0,
+      done: 0
+    },
+    'MAP': {
+      count: 0,
+      done: 0
+    },
+    'INIT_AFTER_MAP_LOADED': {
+      count: 0,
+      done: 0
+    },
+    'INIT_SESSION': {
+      count: 0,
+      done: 0
+    }
+  };
+
+  const handlers = {
+    startInitFunctionOrder: (data) => {
+      // Reconnecting
+      if (data.type == 'INIT_SESSION' && states['INIT_BEFORE_MAP_LOADED'].count < 1) {
+        states['INIT_BEFORE_MAP_LOADED'].count = 1;
+        states['INIT_BEFORE_MAP_LOADED'].done = 1;
+        states['MAP'].count = 1;
+        states['MAP'].done = 1;
+        states['INIT_AFTER_MAP_LOADED'].count = 1;
+        states['INIT_AFTER_MAP_LOADED'].done = 1;
+      }
+  
+      states[data.type].count += data.count;
+    },
+    initFunctionInvoked: (data) => states[data.type].done++,
+    startDataFileEntries: (data) => states['MAP'].count = data.count,
+    performMapLoadFunction: (data) => states['MAP'].done++
+  };
+
+  window.addEventListener('message', (e) => (handlers[e.data.eventName] || (() => {}))(e.data));
+
+  let last = 0;
+
   useEffect(() => {
+    setInterval(() => {
+      let progress = 0;
+      for (let type in states) {
+        if (states[type].done < 1 || states[type].count < 1) continue;
+        progress += (states[type].done / states[type].count) * 100;
+      }
+    
+      let total = Math.min(Math.round(progress / Object.keys(states).length), 100);
+      if (total < last) total = last;
+      last = total;
+      setLoading(total)
+    }, 100)
     window.addEventListener('keydown', handleKeyDown);
 
     // cleanup this component
@@ -76,6 +132,9 @@ function App() {
             >
               <PlayPause buttonToShow={showPlayButton ? "play" : "pause"}/>
           </button>
+        </div>
+        <div className="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700">
+          <div className="h-6 bg-blue-600 rounded-full dark:bg-blue-500" style={{width: `${loading}%`}}>{loading}%</div>
         </div>
       </div>
       
